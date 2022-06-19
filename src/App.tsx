6a3f91b8c497ef1031687f7b1ff5c6b8c6ce2b3e
alikan97 <alikhanhassan2@gmail.com>
2022-06-19 15:40:07 +1000

    more commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import Header from './views/components/header';
 import { Route, Routes } from 'react-router-dom';
 import {AllItemsPage,RequestItemPage, MyItemsPage, LoginPage, RegisterPage} from './views/pages/index';
+import { useAuth } from './hooks/use-auth';
+import { AuthStatus } from './types/auth';
+
+//TODO: DATA VIZ (D3)
 
 function App() {
+  const {state} = useAuth();
+
   return (
     <div className="App font-main">
       <Header />
       <Routes>
         <Route path="/" element={<AllItemsPage/>}/>
         <Route path="/create-item" element={<RequestItemPage/>}/>
-        <Route path='/my-items' element={<MyItemsPage/>}/>
+        {state?.status === AuthStatus.AUTHENTICATED ? 
+        <Route path='/add-role' element={<MyItemsPage/>}/>
+        : null}
         <Route path="/login" element={<LoginPage/>}/>
         <Route path='/register' element={<RegisterPage/>}/>
       </Routes>
